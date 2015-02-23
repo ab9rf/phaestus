81f3465d3f74bcb228c448d746630fc87c5c3433
Kelly Martin <kelly.lynn.martin@gmail.com>
2015-02-23 12:47:25 -0600

    More unit tests of the lexer.

--- src/test/main.hs
+++ src/test/main.hs
@@ -8,6 +8,33 @@ main :: IO ()
 main = hspec $ do 
     describe "tokenizer" $ do
         it "empty file" $ do
-            x <- return $ T.tokenize ""
-            [T.EOF] `shouldBe` x
+            T.tokenize "" `shouldBe` [T.EOF]
+        it "plain html" $ do
+            T.tokenize "plain text" `shouldBe` [T.InlineHTML "plain text", T.EOF] 
+        it "empty code" $ do
+            T.tokenize "<? ?>" `shouldBe` [T.Semicolon, T.EOF] 
+        it "unclosed script block" $ do
+            T.tokenize "<?" `shouldBe` [T.EOF]
+        it "int casts" $
+            T.tokenize "<? (int) (integer)" `shouldBe` [T.CastInt, T.CastInt, T.EOF]
+        it "float casts" $
+            T.tokenize "<? (real) (float) (double)" 
+                `shouldBe` [T.CastReal, T.CastReal, T.CastReal, T.EOF]
+        it "boolean casts" $
+            T.tokenize "<? (bool) (boolean)"
+                `shouldBe` [T.CastBool, T.CastBool, T.EOF]
+        it "other casts" $
+            T.tokenize "<? (string) (array) (object) (unset)"
+                `shouldBe` [T.CastString, T.CastArray, T.CastObject, T.CastUnset, T.EOF]
+        it "==" $
+            T.tokenize "<? ==" `shouldBe` [T.OpEqEq, T.EOF]
+        it "===" $
+            T.tokenize "<? ===" `shouldBe` [T.OpEqEqEq, T.EOF]
+        it "!=" $
+            T.tokenize "<? !=" `shouldBe` [T.OpNotEq, T.EOF]
+        it "<>" $
+            T.tokenize "<? <>" `shouldBe` [T.OpNotEq, T.EOF]
+        it "!==" $
+            T.tokenize "<? !==" `shouldBe` [T.OpNotEqEq, T.EOF]
+            
             
